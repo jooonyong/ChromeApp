6850312bd35caa0b688bf0bee8fd8f3a8272b86c
jooonyong <leejy0718@naver.com>
2021-07-03 17:28:54 +0900

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
-function onLoginSubmit()
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event)
 {
-   const username = loginInput.value;
-   console.log(username);
+   event.preventDefault(); 
+   loginForm.classList.add(HIDDEN_CLASSNAME);
+   localStorage.setItem(USERNAME_KEY,loginInput.value);
+   paintGreetings();
 }
 
-loginForm.addEventListenter("submit",onLoginSubmit);
+loginForm.addEventListener("submit",onLoginSubmit);
+
+function paintGreetings()
+{
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername ===NULL)
+{
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener()
+}else{
+   paintGreetings();
+}
